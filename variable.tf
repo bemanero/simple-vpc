c020a16cdf3cccf8223d5af754cf815d365e98e9
bemanero <emmanuelbobieansah@hotmail.com>
2024-04-10 20:46:15 +0100

    Revert "first commit"
    This reverts commit 0f49599a5aae68b997f4a446ff5c51a8c897acb7.

--- variable.tf
+++ variable.tf
@@ -15,3 +15,40 @@ variable "VPC_cidr_block" {
   description   = "VPC cidr_block"
   default       = "10.0.0.0/16"
 }
+
+variable "Prod-pub-sub1_cidr_block" {
+
+  description   = "subnet cidr_block"
+  default       = "10.0.1.0/24"
+}
+
+variable "Prod-pub-sub2_cidr_block" {
+
+  description   = "subnet cidr_block"
+  default       = "10.0.2.0/24"
+}
+
+variable "Prod-priv-sub1_cidr_block" {
+
+  description   = "subnet cidr_block"
+  default       = "10.0.3.0/24"
+}
+
+variable "prod-priv-sub2_cidr_block" {
+
+  description   = "subnet cidr_block"
+  default       = "10.0.4.0/24"
+}
+
+variable "traffic_route" {
+
+  description   = "traffic route"
+  default       = "local"
+}
+
+variable "public_traffic_route" {
+
+  description   = "public traffic"
+  default       = "0.0.0.0/0"
+
+}
